9a5c1fb477eb8372f0d80fa13025c1a9c1b2760b
Daniel61538 <d.szypniewski.kontakt@gmail.com>
2022-01-31 05:00:26 +0100

    cdn.
    Do dokończenia, już nie myślę

--- ćw4/Main.java
+++ ćw4/Main.java
@@ -1,51 +1,52 @@
-import java.util.Random;
+import java.util.Scanner;
 
-public class Main {
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+        Scanner input = new Scanner(System.in);
+        int[] liczby = new int[]{1, 2, 3, 4, 5, 6};
+        boolean isIndexIncorrect = false;
+        int index = 0;
+            
 
-    public static void main(String[] args) {
-        /* cw.4  */
 
-        /* zad.1 napisać 3 pętle for i użyc:
-        a) w pierwszej pętli break
-        b) w drugiej pętli continue
-        c) w trzeciej pętli return
-        */
+                System.out.print("Wprowadź numer indeksu do którego chcesz zobaczyć przypisaną wartość: ");
+                while (true) 
+                {
+                    try 
+                    {
+                        while (!isIndexIncorrect)
+                        {
+                            index = Integer.parseInt(input.next());
+                            System.out.println("Numer: " + index + "\nWartość: " + liczby[index]);
+                        }
+                    }
+                    catch (ArrayIndexOutOfBoundsException e) 
+                    {
+                        System.out.println("Wprowadź poprawny indeks!");
+                        isIndexIncorrect = true;
+                        continue;
+                    }
+                
+                    catch (NumberFormatException s)
+                    {
+                        System.out.println("Wprowadzony index musi być wartością int");
+                        isIndexIncorrect = true;
+                        continue;
+                    }
+                    finally 
+                    {
+                        System.out.println("Koniec wyświetlania");
+                    }
+                    break; 
 
-        Random rand = new Random();
-        int k = 0;
-        int a = 5;
-        int b = 0;
+                }  
 
-        System.out.println("======================== BREAK ==========================");
-        for (;;) {
-            int x = rand.nextInt(100);
-            if (x > 25) {
-                System.out.println("Wylosowano liczbe: " + x);
-            } else {
-                System.out.println("Wylosowano liczbę mniejszą niż 25, przerwano losowanie!");
-                break;
-            }
-        }
 
-        System.out.println("======================= CONTINUE ========================");
-        for (int i = 0;i < 10;i++) {
-            if (i % 2 == 0) 
-                continue;
-            else {
-                k += i;
-                System.out.println("k jest równe: " + k);
-            }
-        }
 
-        System.out.println("======================== RETURN =========================");
-        for (int i = 0; i < a; i++) {
-            b += a;
-            System.out.println("b jest równe: " + b);
-            if (b == 25){
-                System.out.println("=========================================================");
-                return;
-            } 
-        }
-        System.out.println("jakiś kod, który nigdy nie zostanie wykonany :(");
+
+
+        input.close();
     }
 }
